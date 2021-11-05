11e3e8c95ef1383a45e4503800df8e1c284fb375
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2021-11-05 21:37:41 +0100

    install vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-require('./bootstrap');
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+import Home from './views/Home.vue'
+
+require('./bootstrap')
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', name: 'Home', component: Home },
+    ]
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app')
