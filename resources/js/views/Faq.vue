ff0d7817664c5c295e55fa59228c31e071c2402b
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2021-11-07 03:46:58 +0100

    faq view

--- resources/js/views/Faq.vue
+++ resources/js/views/Faq.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="bg-linen h-full text-black">
-        <div class="border-b-2 last:border-0 border-black p-4" v-for="i in 10" :key="`faq_${i}`">
-            <h3 @click="selected = selected !== i ? i : null" class="cursor-pointer text-center text-lg uppercase">Kto stojí za Spoločnosťou Jaromíra Krejcara?</h3>
+    <div class="bg-linen h-full text-black" v-if="faq">
+        <div class="border-b-2 last:border-0 border-black" v-for="(article, i) in faq" :key="`faq_${i}`">
+            <h3 @click="selected = selected !== i ? i : null" class="cursor-pointer p-4 text-center text-lg uppercase">{{ article.title[$i18n.locale] }}</h3>
         
             <p :class="selected !== i ? 'hidden' : ''" class="text-center">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie arcu eu odio suscipit auctor. Nam fringilla tellus sed elit ultrices, pharetra pretium tellus dapibus. Morbi consequat nec ante sollicitudin aliquam. In ante mi, euismod quis ipsum sit amet, sagittis feugiat diam. Integer vitae fermentum felis. Suspendisse ac lobortis neque. Duis leo leo, tempor eget nisl ut, volutpat convallis nisi. Vestibulum id laoreet metus. Sed mollis libero sed ligula euismod, suscipit tincidunt purus sagittis. Nullam sed porttitor eros, nec fringilla sapien. Morbi nec lectus at urna cursus luctus id in ante. Mauris non convallis urna, vitae dapibus nulla. Mauris sodales, tellus quis tincidunt pharetra, sem ligula dapibus dolor, id consequat felis urna sed massa. Duis urna odio, condimentum non orci in, tempus dictum velit. Nam vitae augue mauris. Ut et euismod mi, id maximus urna.
+                {{ article.content[$i18n.locale] }}
             </p>
         </div>
     </div>
@@ -15,7 +15,13 @@ export default {
     data() {
         return {
             selected: null,
+            faq: [],
         }
-    }
+    },
+    mounted() {
+        axios.get(`/api/faq`).then(({data}) => {
+            this.faq = data
+        })
+    },
 }
 </script>
