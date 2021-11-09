b2af304f8d6cd9b05b8a0793eb6c6ce4a624cdc2
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2021-11-09 12:10:28 +0100

    fix images component

--- resources/js/components/Images.vue
+++ resources/js/components/Images.vue
@@ -1,5 +1,5 @@
-<template v-if="images && images.length">
-    <div class="mb-2">
+<template>
+    <div class="mb-2" v-if="images && images.length">
         <div class="w-full" :class="images.length > 1 ? 'cursor-pointer' : ''" style="aspect-ratio: 16/9;" @click="currentImage = (currentImage + 1) % images.length">
             <img v-for="(image, i) in images" :key="`image_${i}`" v-show="i === currentImage" :src="image.url" :srcset="image.srcset" :alt="image.name" class="block h-full object-contain object-left-top">
         </div>
