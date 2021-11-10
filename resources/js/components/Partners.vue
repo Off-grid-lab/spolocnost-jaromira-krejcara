ccd7d430d077e0b7657bd8073cc9d11bb206e94f
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2021-11-10 13:13:14 +0100

    partners view

---
+++ resources/js/components/Partners.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="border-black border-t-1 px-4 py-8 lg:py-16 text-center text-xl lg:text-2xl">
+        <h2 class="mb-4 lg:mb-8 uppercase">{{ heading }}</h2>
+
+        <div v-for="(item, i) in list" :key="`item_${i}`" class="my-1">
+            <a :href="item.link[$i18n.locale]" class="hover:underline" target="_blank" rel="noopener">{{ item.title[$i18n.locale] }}</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['heading', 'list'],
+}
+</script>>
--- resources/js/views/Team.vue
+++ resources/js/views/Team.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="bg-linen h-full p-4 text-black">
-        <div class="flex flex-wrap -mx-1">
-            <div v-for="(member, i) in list" :key="`member_${i}`" class="block duration-300 group px-1 text-sm w-full lg:w-1/4">
+    <div class="bg-linen h-full text-black">
+        <div class="flex flex-wrap -mx-1 p-4">
+            <div v-for="(member, i) in members" :key="`member_${i}`" class="block duration-300 group px-1 text-sm w-full lg:w-1/4">
                 <div class="mb-2">{{ member.name }}</div>
                 <div style="aspect-ratio: 16/9;">
                     <img v-if="member.image" :src="member.image.url" :srcset="member.image.srcset" :alt="member.image.name" class="block h-full object-cover w-full">
@@ -9,21 +9,33 @@
                 <div class="mb-4 mt-3 min-h-[10rem] w-full">{{ member.content[$i18n.locale] }}</div>
             </div>
         </div>
+
+        <Partners :list="partners" heading="Partneri" />
+        <Partners :list="supports" heading="Podpora &amp; spolupráca" />
     </div>
 </template>
 
 <script>
-import Accordion from '../components/Accordion.vue'
+import Partners from '../components/Partners.vue'
+
 export default {
-    components: { Accordion },
+    components: { Partners },
     data() {
         return {
-            list: [],
+            members: [],
+            partners: [],
+            supports: [],
         }
     },
     mounted() {
         axios.get(`/api/members`).then(({ data }) => {
-            this.list = data.data
+            this.members = data.data
+        })
+        axios.get(`/api/partners`).then(({ data }) => {
+            this.partners = data
+        })
+        axios.get(`/api/supports`).then(({ data }) => {
+            this.supports = data
         })
     }
 }
