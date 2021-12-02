1640538dce0bad84d0bb3f96c72a3e94dcd09d30
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2021-12-02 23:37:33 +0100

    [L] heading space

--- resources/js/components/Images.vue
+++ resources/js/components/Images.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="mb-2" v-if="images && images.length">
+    <div class="mb-10 relative" v-if="images && images.length">
         <div :class="{ 'cursor-pointer': images.length > 1 }" style="aspect-ratio: 3/2;" @click="currentImage = (currentImage + 1) % images.length">
             <img v-for="(image, i) in images" :key="`image_${i}`"
                 v-show="i === currentImage"
@@ -9,7 +9,7 @@
                 class="block h-full"
                 :class="cover ? 'object-cover' : 'object-contain object-left-top'">
         </div>
-        <div class="text-center mt-1" v-if="images.length > 1">{{ currentImage + 1 }}/{{ images.length }}</div>
+        <div class="absolute text-center mt-1 w-full" v-if="images.length > 1">{{ currentImage + 1 }}/{{ images.length }}</div>
     </div>
 </template>
 
