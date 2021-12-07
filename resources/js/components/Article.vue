4e0714a2337e9cc32bf0e694a3b4e5ebf827dc08
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2021-12-07 21:08:40 +0100

    heading margin

--- resources/js/components/Article.vue
+++ resources/js/components/Article.vue
@@ -1,6 +1,6 @@
 <template>
     <article>
-        <h2 v-if="heading" class="font-display leading-none mb-4 pt-16 lg:pt-24 px-[4vw] lg:px-[2vw] text-[15vw] lg:text-[7.5vw] text-center uppercase">{{ heading }}</h2>
+        <h2 v-if="heading" class="font-display leading-none mb-4 mt-24 lg:mt-32 px-[4vw] lg:px-[2vw] text-[15vw] lg:text-[7.5vw] text-center uppercase">{{ heading }}</h2>
 
         <Images v-bind="{ images, cover }" />
 
