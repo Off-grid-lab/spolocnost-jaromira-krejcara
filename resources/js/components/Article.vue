da9294a52ee3efb7d103ae46af2049d86e5f4d48
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2021-12-08 09:16:22 +0100

    heading padding

--- resources/js/components/Article.vue
+++ resources/js/components/Article.vue
@@ -1,6 +1,6 @@
 <template>
     <article>
-        <h2 v-if="heading" class="font-display leading-none mb-4 mt-24 lg:mt-32 px-[4vw] lg:px-[2vw] text-[15vw] lg:text-[7.5vw] text-center uppercase">{{ heading }}</h2>
+        <h2 v-if="heading" class="font-display leading-none m-0 mt-12 lg:mt-24 pb-[2vw] lg:pb-[1vw] pt-[4vw] lg:pt-[2vw] px-[4vw] lg:px-[2vw] text-[15vw] lg:text-[7.5vw] text-center uppercase">{{ heading }}</h2>
 
         <Images v-bind="{ images, cover }" />
 
